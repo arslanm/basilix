d495fcc40fce58a1e7d4b09928b3efff0a4b7302
Murat Arslan <arslanm@gmail.com>
2004-08-09 16:19:17 -0700

    gitdump
    Signed-off-by: arslanm <arslanm@gmail.com>

---
+++ lang/hu/menu.msg.php
@@ -0,0 +1,14 @@
+<?
+// Menu messages
+$lang_msg["menu"] = array(
+	101	=>	'Bejövõ',
+	102	=>	'Mappák',
+	103	=>	'Keresés',
+	104	=>	'Új üzenet',
+	105	=>	'Címjegyzék',
+	106	=>	'Beálllítás',
+	107	=>	'Kilépés',
+	108	=>	'Kimenõ',
+	109	=>	'Kuka'
+);
+?>
