2d740b7935ba0fb6deb7dbe3cdd5244f44313b93
Murat Arslan <arslanm@gmail.com>
2004-08-09 16:19:17 -0700

    gitdump
    Signed-off-by: arslanm <arslanm@gmail.com>

---
+++ lang/hu/settings.msg.php
@@ -0,0 +1,30 @@
+<?
+// 'Settings' messages
+$lang_msg["settings"] = array(
+	501	=>	'Nem tudtam menteni az új beállításokat, próbáld meg késõbb.',
+	502	=>	'Az új beállítások mentve lettek.',
+	503	=>	'Név',
+	504	=>	'(A kimenõ levelek <u>Feladó</u> részénél jelenik meg)',
+	505	=>	'Aláírás',
+	506	=>	'(hozzáfûzheted a kimenõ leveleid végéhez)',
+	507	=>	'Nyelv',
+	508	=>	'(alapértelmezett nyelv)',
+	509	=>	'Üzenetek rendezése: ',
+	510	=>	'(alapértelmezett rendezési elv)',
+	511	=>	'Fordított sorrend',
+	512	=>	'(Igen esetén a legfrissebb levelek kerülnek a lista tetejére)',
+	513	=>	'A kimenõ üzenetek mentése',
+	514	=>	'(Igen esetén az elküldött levelek az "Elküldött levelek" mappában tárolódnak)',
+	515	=>	'Biztonságos törlés',
+	516	=>	'(Igen esetén a törölt levelek a Kukába kerülnek)',
+	517	=>	'Oldal mérete',
+	518	=>	'(az oldalanként megjelenõ üzenetek számát tudod itt meghatározni)',
+	519	=>	'Dátum',
+	520	=>	'Feladó',
+	521	=>	'Tárgy',
+	522	=>	'Méret',
+	523	=>	'Stílus',
+	524	=>	'(A levelezõ kinézetét tudod megváltoztatni itt.)',
+	525	=>	'Beállítások mentése'
+);
+?>
