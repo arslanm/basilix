5db1911826f94230c576b40ae1fa6de4867cc84b
Murat Arslan <arslanm@gmail.com>
2004-08-09 16:19:17 -0700

    gitdump
    Signed-off-by: arslanm <arslanm@gmail.com>

---
+++ lang/hu/findmsg.msg.php
@@ -0,0 +1,18 @@
+<?
+// 'Find message' messages
+$lang_msg["findmsg"] = array(
+	601	=>	'Nem adtad meg, hogy a levelek mely részeiben keressek.',
+	602	=>	'Nem adtál meg keresési feltételt.',
+	603	=>	'Legalább 3 karaktert adjál meg.',
+	604	=>	'Nincsen találat',
+	605	=>	'Találat: %d',
+	606	=>	'Keresés<br><br>Kereshetsz a leveleid között... ',
+	607	=>	'Tartalom',
+	608	=>	'Tárgy',
+	609	=>	'Feladó',
+	610	=>	'<nem használt>',
+	611	=>	'Keresés',
+	612	=>	'Nem választottál ki egyetlen mappát sem.',
+	613	=>	'Dátum'
+);
+?>
