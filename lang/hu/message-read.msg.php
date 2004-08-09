39bb2878bebd421da815ae572090072370bd8e5e
Murat Arslan <arslanm@gmail.com>
2004-08-09 16:19:17 -0700

    gitdump
    Signed-off-by: arslanm <arslanm@gmail.com>

---
+++ lang/hu/message-read.msg.php
@@ -0,0 +1,35 @@
+<?
+// Messages related with reading a message
+$lang_msg["msgs"] = array(
+	251	=>	'Feladó:',
+	252	=>	'Címzett:',
+	253	=>	'Másolat:',
+	254	=>	'Tárgy:',
+	255	=>	'Dátum:',
+	256	=>	'Üzenet:',
+	257	=>	'Mellékletek:',
+	258	=>	'<div align="center"><b>A levél nem tartalmaz semmit.</b></div>',
+	259	=>	'A keresett levél nem található; lehet, hogy törölve lett...?!',
+	260	=>	'Elõzõ',		// unused
+	261	=>	'Következõ',		// unused
+	262	=>	'Válasz',
+	263	=>	'Válasz mindenkinek',
+	264	=>	'Törlés',
+	265	=>	'Továbbítás',
+	266	=>	'Fejléc',
+	267	=>	'Nincsen tárgy',
+	268	=>	'Cím mentése',
+	269	=>	'HTML üzenet',
+	270	=>	'RFC822 üzenet',
+	271	=>	'Kézbesítési állapot',	//vagy állapotjelentés lesz
+	272	=>	'Plaintext üzenet',
+	273	=>	'Enriched Text üzenet',
+	274	=>	'Ismeretlen melléklet',
+	275	=>	'Text üzenet',
+	276	=>	'Alkalmazás',
+	277	=>	'Zenei file',
+	278	=>	'Kép file',
+	279	=>	'Film',
+	280	=>	'PGP aláírás'
+);
+?>
