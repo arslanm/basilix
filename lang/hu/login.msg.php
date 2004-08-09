730a9efd5d928b39c28d3ae9fb5abf4fe85be755
Murat Arslan <arslanm@gmail.com>
2004-08-09 16:19:17 -0700

    gitdump
    Signed-off-by: arslanm <arslanm@gmail.com>

---
+++ lang/hu/login.msg.php
@@ -0,0 +1,16 @@
+<?
+// Login messages
+$lang_msg["login"] = array(
+	51	=>	'E-mail cím:',
+	52	=>	'Jelszó:',
+	53	=>	'Belépés',
+	54	=>	'Ha ismét be szeretnél lépni, írd be a jelszavadat',
+	55	=>	'Felhasználó váltás',
+	56	=>	'Biztonságos kapcsolat (lassú)',
+	57	=>	'Bizonytalan kapcsoalt',
+	58	=>	'Kérlek töltsd ki az össze mezõt.',
+	59	=>	'Kérlek ellenõrizd az e-mail címet.',
+	60	=>	'Kérlek ellenõrizd a jelszót.',
+	61	=>	'Hibás jelszót vagy nem létezõ e-mail címet adtál meg.'
+);
+?>
