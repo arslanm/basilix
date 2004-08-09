43ac9e9d8988e577c628c6e97ee2c151eae6882f
Murat Arslan <arslanm@gmail.com>
2004-08-09 16:19:17 -0700

    gitdump
    Signed-off-by: arslanm <arslanm@gmail.com>

---
+++ lang/hu/global.msg.php
@@ -0,0 +1,15 @@
+<?
+// Global messages
+$lang_msg["global"] = array(
+	1	=>	'Igen',
+	2	=>	'Nem',
+	3	=>	'Kérlek válassz',
+	4	=>	'Engedélyezdned kell a JavaScript támogatást a böngészõdben...',
+	5	=>	'Engedélyezdned kell a CSS támogatást a böngészõdben...',
+	6	=>	'Engedélyezdned kell a Cookie támogatást a böngészõdben...',
+	7	=>	'Ismeretlen',
+	8	=>	'Nyomtatás',
+	9	=>	'Ablak bezárása',
+	10	=>	'Jelenleg %d felhasználó van belépve'
+);
+?>
